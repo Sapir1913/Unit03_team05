1ff9e15bf3a96c3e2880b398c3de7ed47d82d768
Sapir1913 <shohamsapir221@gmail.com>
2026-01-02 14:58:01 +0200

    Update Unit03.java

--- Unit03.java
+++ Unit03.java
@@ -1,7 +1,24 @@
 public class Unit03{  
     
-    public static int shiftRight(int num)    {
+    // Shoham
+public static int shiftRight(int num) {
+    if (num < 0 || num > 9999) {
         return -1;
+    }
+
+    if (num < 10) {
+        return num;
+    }
+
+    int lastDigit = num % 10;
+    num = num / 10;
+
+    int length = (int) Math.log10(num) + 1;
+    int shifted = lastDigit * (int) Math.pow(10, length) + num;
+
+    return shifted;
+}
+
     }
     public static int shiftRight(int num, int k) {
     
@@ -34,11 +51,28 @@ public class Unit03{
 
     return rest * 10 + firstDigit;
     }
+    // Shoham
     public static int shiftLeft(int num, int k){
+    if ((num > 9999 || num < 1000) || (k < 1 || k > 4)){
         return -1;
+        }
+    String sNum = String.valueOf(num);
+    String leftPart = sNum.substring(0, k);
+    String rightPart = sNum.substring(k);
+    String shifted = rightPart + leftPart;
+    return Integer.parseInt(shifted); 
     }
-    public static String shiftRight(String s)    {
-        return "";
+    }
+     // Shoham
+     public static String shiftRight(String s){
+    int lngth = s.length();
+    if (lngth < 2){
+    return s;
+    }
+    String lastChar = s.substring(lngth - 1);
+    String leftPart = s.substring(0, lngth - 1);
+    String shifted = lastChar + leftPart;
+    return shifted;
     }
     public static String shiftLeft(String s)    {
         return "";
